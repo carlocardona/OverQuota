6cf1bda01e1ee9cd98bc3f21af7de0a0f338e4ba
Carlo <carlo.dev2@gmail.com>
2021-03-03 11:38:24 +1100

    changed search bar bg

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import './SearchBar.css'
 
 const SearchBar = () => {
     return (
-        <div>
+        <div className="container">
             <h3>Search Bar</h3>
         </div>
     )
